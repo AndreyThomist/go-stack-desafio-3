15875c505a533a08d86f1adb490dec82fb625996
Andrey <andrey.moraes@moraesvelleda.com.br>
2020-11-12 22:15:06 -0300

    desafio 3 omnistack bootcamp

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from "react";
-
+import React,{useState,useEffect} from "react";
+import api from './services/api'
 import "./styles.css";
 
 function App() {
+  const [repositories,setRepositories] = useState([])
   async function handleAddRepository() {
-    // TODO
+    const response =  await api.post('/repositories',{
+      title:new Date().getTime() + 'React',
+      url:'www.google.com',
+      techs:['vue','react']
+    })
+    setRepositories([...repositories,response.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+     const response = await api.delete(`/repositories/${id}`)
+     const filteredRepositories = repositories.filter(repository => repository.id !== id);
+     setRepositories([...filteredRepositories]);
   }
+
+  useEffect(() => {
+    api.get('/repositories').then(response => {
+      setRepositories(response.data);
+    })
+ })
 
   return (
     <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+      <ul>
+         {repositories.map(repository => {
+          return <li  key={repository.id}>
+           {repository.title}
+           <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+           </button>
+         </li>
+         })}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
